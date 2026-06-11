010f43ec5928fbc46d9b5ac7686978b62df8c88a
Nicole Jean Reconasa <nicolejeangilpareconasa@gmail.com>
2026-06-11 11:05:52 +0800

    Add files via upload

---
+++ book.py
@@ -0,0 +1,26 @@
+class Book:
+    def __init__(self, book_id, title, author):
+        self._book_id = book_id
+        self._title = title
+        self._author = author
+        self._is_available = True
+
+    def borrow(self):
+        if self._is_available:
+            self._is_available = False
+        else:
+            print("Book is already borrowed.")
+
+    @property
+    def is_available(self):
+        return self._is_available
+
+    def return_book(self):
+        self._is_available = True
+
+    @property
+    def book_id(self):
+        return self._book_id
+
+    def display_info(self):
+        print(f"{self._book_id} | {self._title} | {self._author} | Available: {self._is_available}")
